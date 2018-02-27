04a57b571fa1c2e868dbe0eea9186597cd0956b6
Morteza Nourelahi Alamdari <me@mortezana.com>
2018-02-27 15:46:18 +0330

    Add login handler and methods.

--- ibsng/handler/login/search_admin_login_history.py
+++ ibsng/handler/login/search_admin_login_history.py
@@ -1,24 +1,36 @@
-""" info API method."""
+"""Search admin login history API method."""
 from ibsng.handler.handler import Handler
 
 
 class searchAdminLoginHistory(Handler):
-    """ info method class."""
+    """Search admin login history method class."""
 
-    def setup(self, conds, _from, to_, sort_by, desc):
+    def control(self):
+        """Validate inputs after setup method.
+
+        :return: None
+        :rtype: None
+        """
+        self.is_valid(self.conds, dict)
+        self.is_valid(self.from_index, int)
+        self.is_valid(self.to_index, int)
+        self.is_valid(self.sort_by, str)
+        self.is_valid(self.desc, bool)
+
+    def setup(self, conds, from_index, to_index, sort_by, desc):
         """Setup required parameters.
 
-        :param dict conds: 
-        :param int _from: 
-        :param int to: 
-        :param choice sort_by: 
+        :param dict conds: conditions
+        :param int from_index: from index
+        :param int to_index: end index
+        :param str sort_by: sort result by specific field
         :param bool desc: descending
-    
-        :return: void
-        :rtype: void
+
+        :return: None
+        :rtype: None
         """
         self.conds = conds
-        self._from = _from
-        self.to_ = to_
+        self.__from = from_index
+        self.to_ = to_index
         self.sort_by = sort_by
         self.desc = desc

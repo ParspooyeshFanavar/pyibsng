c9b0e8dd630c1ac4be0131a5c788709284cc8ffd
Morteza Nourelahi Alamdari <me@mortezana.com>
2018-02-27 14:51:38 +0330

    Add 'voip provider' handler and methods.

--- ibsng/handler/voip_provider/list_vo_i_p_providers.py
+++ ibsng/handler/voip_provider/list_vo_i_p_providers.py
@@ -1,17 +1,8 @@
-""" info API method."""
+"""List voip providers API method."""
 from ibsng.handler.handler import Handler
 
 
 class listVoIPProviders(Handler):
-    """ info method class."""
+    """List VOIP providers method class."""
 
-    def setup(self, **kwargs):
-        """Setup required parameters.
-
-        :param dict kwargs: input args
-
-        :return: void
-        :rtype: void
-        """
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    pass
